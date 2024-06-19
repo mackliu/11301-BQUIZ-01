f67c83e6d9049e637bcbad1bd78a8983c1494503
kai <cgk0910@gmail.com>
2024-06-19 08:42:54 +0800

    修改後端api程式以符合不同資料表的欄位需求

--- api/edit.php
+++ api/edit.php
@@ -8,13 +8,37 @@ foreach ($_POST['id'] as $key => $id) {
         $db->del($id);
     } else {
         $row = $db->find($id);
+
+        /* switch($do){
+            case 'admin':
+            break;
+            case 'menu':
+            break;
+            case 'title':
+            break;
+            default:
+        } */
+
         if (isset($_POST['text'])) {
             $row['text'] = $_POST['text'][$key];
         }
         if ($do == 'title') {
             $row['sh'] = (isset($_POST['sh']) && $_POST['sh'] == $id) ? 1 : 0;
         } else {
-            $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
+            if (isset($row['sh'])) {
+                $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
+            } else {
+                switch ($do) {
+                    case 'admin':
+                        $row['acc'] = $_POST['acc'][$key];
+                        $row['pw'] = $_POST['pw'][$key];
+                        break;
+                    case 'menu':
+                        $row['href'] = $_POST['href'][$key];
+                        $row['text'] = $_POST['text'][$key];
+                        break;
+                }
+            }
         }
 
         $db->save($row);
